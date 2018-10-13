43f24f4f1e8f28d6c866e08da7a66a751f5c4142
KevinStill <42301406+KevinStill@users.noreply.github.com>
2018-10-13 11:02:46 -0700

    carousel nav update

--- myScript.js
+++ myScript.js
@@ -1,9 +1,21 @@
-$(document).ready(function() {
+$(document).ready(function () {
   $('#mycarousel').carousel({ interval: 2000 });
-  $('#carousel-pause').click(function() {
-    $('#mycarousel').carousel('pause');
-  });
-    $('#carousel-play').click(function() {
+
+  $('#carouselButton').click(function () {
+
+    if ($('#carouselButton').children('span').hasClass('fa-pause')) {
+      $('#mycarousel').carousel('pause');
+      $('#carouselButton').children('span').removeClass('fa-pause');
+      $('#carouselButton').children('span').addClass('fa-play');
+    }
+    else {
       $('#mycarousel').carousel('cycle');
+      $('#carouselButton').children('span').removeClass('fa-play');
+      $('#carouselButton').children('span').addClass('fa-pause');
+    };
+
+
   });
-  })
+
+
+});
