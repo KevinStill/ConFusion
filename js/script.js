850bee4a8d14e914aaf85bbbb01324b149accd95
KevinStill <42301406+KevinStill@users.noreply.github.com>
2018-10-15 12:11:06 -0700

    end of bootstrap

--- js/script.js
+++ js/script.js
@@ -17,5 +17,31 @@ $(document).ready(function () {
 
   });
 
+  $('#resButton').click(function () {
+    console.log("button click works")
+    $('#resModal').modal('show')
+  });
+ 
+  $('#resClose').click(function () {
+    console.log("login click click works")
+    $('#resModal').modal('hide')
+  });
+  $('#resClose2').click(function () {
+    console.log("login click click works")
+    $('#resModal').modal('hide')
+  });
+
+  $('#loginButton').click(function () {
+    console.log("login click click works")
+    $('#loginModal').modal('show')
+  });
+  $('#loginClose').click(function () {
+    console.log("login click click works")
+    $('#loginModal').modal('hide')
+  });
+  $('#loginClose2').click(function () {
+    console.log("login click click works")
+    $('#loginModal').modal('hide')
+  });
 
 });
